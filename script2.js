9257b573c28cdd818197a6fa105b6b31e579d934
Legit jesus <70650796+katanpink@users.noreply.github.com>
2022-10-12 17:02:48 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,5 @@
+const thang = document.getElementsByClassName('red')
+if (thang.length > 2){
+    var el = thang[2]
+    el.className = 'green'
+}
---
+++ script3.js
@@ -0,0 +1,5 @@
+const vari = document.getElementsByTagName('li');
+if (vari.length > 3){
+    var el = vari[3];
+    el.className = 'green'
+}
